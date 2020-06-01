775190504e67d4e99a535b4943d742f218bd69d4
galkatz373@gmail.com <galkatz373@gmail.com>
2020-06-02 00:10:41 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { useForm } from "./useForm"
 export { useFormContext } from "./useFormContext"
-export { FormContext } from "./FormContext.svelte"
+export { default as FormContext } from "./FormContext.svelte"
 export { default as ErrorMessage } from "./ErrorMessage.svelte"
