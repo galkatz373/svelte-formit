5919a4d58dc372863533acb2581d5419b2fa1962
galkatz373@gmail.com <galkatz373@gmail.com>
2020-06-03 22:47:57 +0300

    allow custom values

--- src/useForm.js
+++ src/useForm.js
@@ -1,81 +1,56 @@
-import { writable, get, derived } from "svelte/store";
-import {
-  getNodeValue,
-  isRadioButton,
-  flatten,
-  unflatten,
-  isSelectMulti,
-  findInputOrSelect,
-} from "./util";
-import { tick } from "svelte";
+import { writable, get, derived } from "svelte/store"
+import { isRadioButton, flatten, unflatten, isSelectMulti, findInputOrSelect } from "./util"
+import { tick } from "svelte"
 
-export const key = {};
+export const key = {}
 
 export const useForm = ({ mode } = {} || undefined) => {
-  let fields = writable({});
-  let errors = writable({});
-  let isSubmitting = writable(false);
-  let touched = writable({});
-  let dirty = writable({});
-  let isValid = derived(errors, ($errors) => Object.keys($errors).length === 0);
-  let validations = writable({});
+  let fields = writable({})
+  let errors = writable({})
+  let isSubmitting = writable(false)
+  let touched = writable({})
+  let dirty = writable({})
+  let isValid = derived(errors, ($errors) => Object.keys($errors).length === 0)
+  let validations = writable({})
+  let values = writable({})
 
-  const register = (node, validate) => {
-    let initialName = node.name || node.getAttribute("name");
-    node = findInputOrSelect(node);
-    node.name = initialName;
-    const { name } = node;
+  const register = (node, validate, isCustom = false) => {
+    let initialName = node.name || node.getAttribute("name")
+    node = findInputOrSelect(node)
+    node.name = initialName
+    const { name } = node
     if (validate) {
-      validations.update((n) => ({ ...n, [name]: { validate, ref: node } }));
+      validations.update((n) => ({ ...n, [name]: { validate, ref: node } }))
     }
 
     if (isRadioButton(node)) {
       fields.update((n) => ({
         ...n,
         [name]: n[name] ? [...n[name], node] : [node],
-      }));
+      }))
+      values.update((n) => node.checked && { ...n, [name]: node.value })
     } else if (isSelectMulti(node)) {
-      fields.update((n) => ({ ...n, [name]: node.options }));
-    } else {
-      fields.update((n) => ({ ...n, [name]: node }));
-    }
-
-    fields.update((n) => {
-      return isRadioButton(node)
-        ? { ...n, [name]: n[name] ? [...n[name], node] : [node] }
-        : { ...n, [name]: node };
-    });
-
-    node.addEventListener("input", (e) => {
-      let value = getNodeValue(node);
-      if (mode && validate && mode === "onChange") {
-        for (let func of Object.values(validate)) {
-          if (func(value)) {
-            errors.update((n) => ({
-              ...n,
-              [name]: {
-                message: func(value),
-                ref: e.target,
-              },
-            }));
-            break;
-          } else {
-            errors.update((n) => {
-              let { [name]: value, ...rest } = n;
-              return rest;
-            });
-          }
+      fields.update((n) => ({ ...n, [name]: node.options }))
+      let selected = []
+      for (let option of node.options) {
+        if (option.selected) {
+          selected.push(option.value)
         }
       }
-    });
+      values.update((n) => ({ ...n, [name]: selected }))
+    } else {
+      fields.update((n) => ({ ...n, [name]: node }))
+      values.update((n) => ({ ...n, [name]: node.value }))
+    }
 
-    node.addEventListener("change", (e) => {
-      fields.update((n) => ({ ...n, [name]: e.target }));
-    });
-
-    if (isRadioButton(node)) {
-      node.addEventListener("click", (e) => {
-        let value = getNodeValue(node);
+    // fields.update((n) => {
+    //   return isRadioButton(node) ? { ...n, [name]: n[name] ? [...n[name], node] : [node] } : { ...n, [name]: node }
+    // })
+    if (!isCustom) {
+      node.addEventListener("input", (e) => {
+        // let value = getNodeValue(node)
+        values.update((n) => ({ ...n, [name]: e.target.value }))
+        let value = get(values)[name]
         if (mode && validate && mode === "onChange") {
           for (let func of Object.values(validate)) {
             if (func(value)) {
@@ -85,21 +60,66 @@ export const useForm = ({ mode } = {} || undefined) => {
                   message: func(value),
                   ref: e.target,
                 },
-              }));
-              break;
+              }))
+              break
             } else {
               errors.update((n) => {
-                let { [name]: value, ...rest } = n;
-                return rest;
-              });
+                let { [name]: value, ...rest } = n
+                return rest
+              })
             }
           }
         }
-      });
+      })
+
+      node.addEventListener("change", (e) => {
+        fields.update((n) => ({ ...n, [name]: node }))
+        if (isRadioButton(node)) {
+          values.update((n) => node.checked && { ...n, [name]: node.value })
+        } else if (isSelectMulti(node)) {
+          let selected = []
+          for (let option of node.options) {
+            if (option.selected) {
+              selected.push(option.value)
+            }
+          }
+          values.update((n) => ({ ...n, [name]: selected }))
+        } else {
+          values.update((n) => ({ ...n, [name]: node.value }))
+        }
+      })
+
+      if (isRadioButton(node)) {
+        node.addEventListener("click", (e) => {
+          // let value = getNodeValue(node)
+          values.update((n) => ({ ...n, [name]: node.value }))
+          let value = get(values)[name]
+          if (mode && validate && mode === "onChange") {
+            for (let func of Object.values(validate)) {
+              if (func(value)) {
+                errors.update((n) => ({
+                  ...n,
+                  [name]: {
+                    message: func(value),
+                    ref: node,
+                  },
+                }))
+                break
+              } else {
+                errors.update((n) => {
+                  let { [name]: value, ...rest } = n
+                  return rest
+                })
+              }
+            }
+          }
+        })
+      }
     }
 
     node.addEventListener("blur", (e) => {
-      let value = node.type === "checkbox" ? e.target.checked : e.target.value;
+      // let value = node.type === "checkbox" ? e.target.checked : e.target.value
+      let value = get(values)[name]
       if (mode && validate && mode === "onBlur") {
         for (let func of Object.values(validate)) {
           if (func(value)) {
@@ -109,160 +129,157 @@ export const useForm = ({ mode } = {} || undefined) => {
                 message: func(value),
                 ref: e.target,
               },
-            }));
-            break;
+            }))
+            break
           } else {
             errors.update((n) => {
-              let { [name]: value, ...rest } = n;
-              return rest;
-            });
+              let { [name]: value, ...rest } = n
+              return rest
+            })
           }
         }
       }
       touched.update((n) => ({
         ...n,
         [name]: e.target,
-      }));
-    });
+      }))
+    })
 
     node.addEventListener("focus", (e) => {
       dirty.update((n) => ({
         ...n,
         [name]: e.target,
-      }));
-    });
+      }))
+    })
 
     return {
       destroy() {
-        unregister(name);
+        unregister(name)
       },
-    };
-  };
+    }
+  }
 
   const unregister = (name) => {
     validations.update((n) => {
-      let { [name]: value, ...rest } = n;
-      return rest;
-    });
+      let { [name]: value, ...rest } = n
+      return rest
+    })
     fields.update((n) => {
-      let { [name]: value, ...rest } = n;
-      return rest;
-    });
+      let { [name]: value, ...rest } = n
+      return rest
+    })
     errors.update((n) => {
-      let { [name]: value, ...rest } = n;
-      return rest;
-    });
+      let { [name]: value, ...rest } = n
+      return rest
+    })
     dirty.update((n) => {
-      let { [name]: value, ...rest } = n;
-      return rest;
-    });
+      let { [name]: value, ...rest } = n
+      return rest
+    })
     touched.update((n) => {
-      let { [name]: value, ...rest } = n;
-      return rest;
-    });
-  };
+      let { [name]: value, ...rest } = n
+      return rest
+    })
+    values.update((n) => {
+      let { [name]: value, ...rest } = n
+      return rest
+    })
+  }
 
   const watch = (name) => {
-    if (get(fields)) return getNodeValue(get(fields)[name]);
-  };
+    if (get(fields)) return get(values)[name]
+  }
 
   const triggerValidation = (name) => {
     if (get(validations)[name]) {
       for (let func of Object.values(get(validations)[name].validate)) {
-        if (func(getNodeValue(get(validations)[name].ref))) {
+        // getNodeValue(get(validations)[name].ref
+        if (func(get(values)[name])) {
           errors.update((n) => ({
             ...n,
             [name]: {
-              message: func(getNodeValue(get(validations)[name].ref)),
+              message: func(get(values)[name]),
               ref: get(validations)[name].ref,
             },
-          }));
-          break;
+          }))
+          break
         } else {
           errors.update((n) => {
-            let { [name]: value, ...rest } = n;
-            return rest;
-          });
+            let { [name]: value, ...rest } = n
+            return rest
+          })
         }
       }
     } else {
-      console.error("This field doesn't have validations or doesn't exist");
+      console.error("This field doesn't have validations or doesn't exist")
     }
-  };
+  }
 
   const setValue = (fieldName, value) => {
-    fields.update((n) => {
-      if (get(fields)[fieldName]) {
-        get(fields)[fieldName].type === "checkbox"
-          ? (get(fields)[fieldName].checked = value)
-          : (get(fields)[fieldName].value = value);
-      }
-
-      return n;
-    });
-  };
+    values.update((n) => ({ ...n, [fieldName]: value }))
+  }
 
   const getValues = () => {
-    let values = {};
-    for (let [key, ref] of Object.entries(get(fields))) {
-      values = {
-        ...values,
-        [key]: getNodeValue(ref),
-      };
-    }
-    return unflatten(values);
-  };
+    // for (let [key, ref] of Object.entries(get(fields))) {
+    //   values = {
+    //     ...values,
+    //     [key]: getNodeValue(ref),
+    //   }
+    // }
+    return unflatten(get(values))
+  }
 
   const reset = (values) => {
-    let flattenValues = flatten(values);
+    let flattenValues = flatten(values)
     for (let [key, value] of Object.entries(flattenValues)) {
-      setValue(key, value);
+      setValue(key, value)
     }
-  };
+  }
 
-  const handleSubmit = (ev, onSubmit) => {
+  const handleSubmit = async (ev, onSubmit) => {
     if (ev && ev.preventDefault) {
-      ev.preventDefault();
+      ev.preventDefault()
     }
+    // getNodeValue(get(fields)[field.ref.name]
     for (let field of Object.values(get(validations))) {
       for (let func of Object.values(field.validate)) {
-        if (func(getNodeValue(get(fields)[field.ref.name]))) {
+        if (func(get(values)[field.ref.name])) {
           errors.update((n) => ({
             ...n,
             [field.ref.name]: {
-              message: func(getNodeValue(get(fields)[field.ref.name])),
+              message: func(get(values)[field.ref.name]),
               ref: field.ref,
             },
-          }));
-          break;
+          }))
+          break
         } else {
           errors.update((n) => {
-            let { [field.ref.name]: value, ...rest } = n;
-            return rest;
-          });
+            let { [field.ref.name]: value, ...rest } = n
+            return rest
+          })
         }
       }
     }
 
-    let firstError = Object.values(get(errors))[0];
+    let firstError = Object.values(get(errors))[0]
     if (firstError) {
-      firstError.ref.focus();
+      firstError.ref.focus()
     }
 
-    isSubmitting.set(true);
+    isSubmitting.set(true)
 
-    let values = {};
+    // let values = {}
 
-    for (let [key, field] of Object.entries(get(fields))) {
-      values = {
-        ...values,
-        [key]: getNodeValue(field),
-      };
-    }
+    // for (let [key, field] of Object.entries(get(fields))) {
+    //   values = {
+    //     ...values,
+    //     [key]: getNodeValue(field),
+    //   }
+    // }
 
-    get(isValid) && onSubmit(unflatten(values));
-    isSubmitting.set(false);
-  };
+    get(isValid) && (await onSubmit(unflatten(get(values))))
+    isSubmitting.set(false)
+  }
 
   return {
     getValues,
@@ -278,5 +295,5 @@ export const useForm = ({ mode } = {} || undefined) => {
     errors,
     unregister,
     reset,
-  };
-};
+  }
+}
--- src/util.js
+++ src/util.js
@@ -1,90 +1,68 @@
-export const getNodeValue = (ref) => {
-  if (Array.isArray(ref)) {
-    let value = "";
-    for (let item of ref) {
-      if (item.checked) value = item.value;
-    }
-    return value;
-  }
-  if (isCheckbox(ref)) return ref.checked;
-  if (isSelectMulti(ref)) {
-    let selected = [];
-    for (let i = 0; i < ref.length; i++) {
-      if (ref.options[i].selected) selected.push(ref.options[i].value);
-    }
-    return selected;
-  }
-  if (isFileInput(ref)) {
-    return ref.files;
-  }
-  return ref.value;
-};
-
-export const isRadioButton = (ref) => ref.type === "radio";
-export const isCheckbox = (ref) => ref.type === "checkbox";
-export const isSelectMulti = (ref) => ref.type === "select-multiple";
-export const isFileInput = (ref) => ref.type === "file";
+export const isRadioButton = (ref) => ref.type === "radio"
+export const isCheckbox = (ref) => ref.type === "checkbox"
+export const isSelectMulti = (ref) => ref.type === "select-multiple"
+export const isFileInput = (ref) => ref.type === "file"
 
 export const flatten = (obj) => {
-  const flattened = {};
+  const flattened = {}
 
   Object.keys(obj).forEach((key) => {
     if (typeof obj[key] === "object" && obj[key] !== null) {
-      Object.assign(flattened, flatten(obj[key]));
+      Object.assign(flattened, flatten(obj[key]))
     } else {
-      flattened[key] = obj[key];
+      flattened[key] = obj[key]
     }
-  });
+  })
 
-  return flattened;
-};
+  return flattened
+}
 
 export function unflatten(table) {
-  var result = {};
+  var result = {}
 
   for (var path in table) {
     var cursor = result,
       length = path.length,
       property = "",
-      index = 0;
+      index = 0
 
     while (index < length) {
-      var char = path.charAt(index);
+      var char = path.charAt(index)
 
       if (char === "[") {
         var start = index + 1,
           end = path.indexOf("]", start),
           cursor = (cursor[property] = cursor[property] || []),
           property = path.slice(start, end),
-          index = end + 1;
+          index = end + 1
       } else {
         var cursor = (cursor[property] = cursor[property] || {}),
           start = char === "." ? index + 1 : index,
           bracket = path.indexOf("[", start),
-          dot = path.indexOf(".", start);
+          dot = path.indexOf(".", start)
 
-        if (bracket < 0 && dot < 0) var end = (index = length);
-        else if (bracket < 0) var end = (index = dot);
-        else if (dot < 0) var end = (index = bracket);
-        else var end = (index = bracket < dot ? bracket : dot);
+        if (bracket < 0 && dot < 0) var end = (index = length)
+        else if (bracket < 0) var end = (index = dot)
+        else if (dot < 0) var end = (index = bracket)
+        else var end = (index = bracket < dot ? bracket : dot)
 
-        var property = path.slice(start, end);
+        var property = path.slice(start, end)
       }
     }
 
-    cursor[property] = table[path];
+    cursor[property] = table[path]
   }
 
-  return result[""];
+  return result[""]
 }
 
 export const findInputOrSelect = (ref) => {
   if (ref.tagName === "INPUT" || ref.tagName === "SELECT") {
-    return ref;
+    return ref
   }
   for (let child of ref.children) {
-    return findInputOrSelect(child);
+    return findInputOrSelect(child)
   }
-};
+}
 
-export const getNodeName = (ref) => ref.name || ref.getAttribute("name");
+export const getNodeName = (ref) => ref.name || ref.getAttribute("name")
